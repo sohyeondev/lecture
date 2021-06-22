2628de3128e162eacff24021224b2e70a931b3b7
root <root@DESKTOP-IE8KBFV.localdomain>
2021-06-22 10:42:16 +0900

    10:42 update

--- node/client_udp.js
+++ node/client_udp.js
@@ -1,8 +1,8 @@
 const dgram = require('dgram');
-const buf1 = Buffer.form('I am Alice');
+const buf1 = Buffer.from('I am Alice');
 const buf2 = Buffer.from(' Or Bob');
 const client = dgram.createSocket('udp4');
-client.sen([buf1, buf2], 41234, '172.19.217.202', (err) => {
+client.send([buf1, buf2], 41234, '172.19.217.202', (err) => {
     if(err){
         console.log(err)
     }
--- node/server_udp.js
+++ node/server_udp.js
@@ -7,11 +7,11 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-    console.log(`serer got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
-    const address = sercer.address();
+    const address = server.address();
     console.log(`server listening ${address.address}:${address.port}`);
 });
 
